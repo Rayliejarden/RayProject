5d3da313cb007e71b4b30ac1389bae2f43776e69
Rayliejarden <jardenraylie7@gmail.com>
2025-07-23 14:08:37 +0800

    first commit

---
+++ src/javaapplication_jarden/JavaApplication_jarden.java
@@ -0,0 +1,23 @@
+
+package javaapplication_jarden;
+
+import java.util.Scanner;
+
+
+public class JavaApplication_jarden {
+
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanners(System.in);
+        
+        String name;
+        int age = 19;
+       System.out.println("rarara");
+       
+      
+
+      
+    }
+    
+}
